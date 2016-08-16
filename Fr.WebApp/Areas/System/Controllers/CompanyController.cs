848a45be851f53c74f167d9af534b296d1354d3a
liyu7342 <liyu7342@gmail.com>
2016-08-17 07:32:16 +0800

    2

--- Fr.WebApp/Areas/System/Controllers/CompanyController.cs
+++ Fr.WebApp/Areas/System/Controllers/CompanyController.cs
@@ -1,9 +1,11 @@
 ﻿using Fr.Dto;
 using Fr.Dto.System;
 using Fr.IAdapter;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -29,10 +31,14 @@ namespace Fr.WebApp.Areas.System.Controllers
         }
 
         [JsonException,HttpGet]
-        public JsonResult GetCompanyList()
+        public ActionResult GetCompanyList()
         {
             var list = _adapter.GetCompanyList();
-            return Json(new {success=true,data=list,rows=list }, JsonRequestBehavior.AllowGet);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{ \"rows\": [");
+            sb.Append(TreeGridJson(list, -1));
+            sb.Append("]}");
+            return Content(sb.ToString());
         }
 
         [JsonException,HttpGet]
@@ -60,6 +66,27 @@ namespace Fr.WebApp.Areas.System.Controllers
         {
             _adapter.Delete(keyId);
             return Json(new JsonResponse() { success=true});
+        }
+
+        
+        int lft = 1, rgt = 1000000;
+        public string TreeGridJson(List<SysCompanyDto> ListData, int index, string ParentId = "0")
+        {
+            StringBuilder sb = new StringBuilder();
+            List<SysCompanyDto> ChildNodeList = ListData.FindAll(t => t.ParentId == ParentId);
+            if (ChildNodeList.Count > 0) { index++; }
+            foreach (SysCompanyDto entity in ChildNodeList)
+            {
+                string strJson =JsonConvert.SerializeObject(entity);
+                strJson = strJson.Insert(1, "\"level\":" + index + ",");
+                strJson = strJson.Insert(1, "\"isLeaf\":" + (ListData.Count<SysCompanyDto>(t => t.ParentId == entity.CompanyId) == 0 ? true : false).ToString().ToLower() + ",");
+                strJson = strJson.Insert(1, "\"expanded\":true,");
+                strJson = strJson.Insert(1, "\"lft\":" + lft++ + ",");
+                strJson = strJson.Insert(1, "\"rgt\":" + rgt-- + ",");
+                sb.Append(strJson);
+                sb.Append(TreeGridJson(ListData, index, entity.CompanyId));
+            }
+            return sb.ToString().Replace("}{", "},{");
         }
     }
 }
